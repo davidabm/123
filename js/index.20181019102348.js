48990501d79e214201c58d553690ec7aa74ae6f7
yuwei <davidabm@163.com>
2018-10-19 10:23:56 +0800

    2

--- js/index.20181019102348.js
+++ js/index.20181019102348.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
 if(! document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")) {
 $('.js').attr('src', 'images/logo-108.png');
 $('.js-2').attr('src', 'images/png-1--37.png');
+$('.js-3').attr('src', 'images/zi-ti-47.png');
+$('.js-4').attr('src', 'images/gang-bi-36.png');
 
 }
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
