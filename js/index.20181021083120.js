edaa85720ed57f9aeca3b27f7a5c1a65aeae674e
david <davidabm@163.com>
2018-11-03 18:30:31 +0800

    add https://file.pizza/

--- js/index.20181021083120.js
+++ js/index.20181021083120.js
@@ -4,13 +4,14 @@ $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof att
 
 $(document).ready(function() {
 if(! document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")) {
-$('.js').attr('src', 'images/logo-108.png');
+$('.js').attr('src', 'images/logo-108-2.png');
 $('.js-2').attr('src', 'images/png-1--37.png');
 $('.js-3').attr('src', 'images/zi-ti-47.png');
 $('.js-4').attr('src', 'images/gang-bi-36.png');
-$('.js-5').attr('src', 'images/yan-se-40.png');
-$('.js-6').attr('src', 'images/sketch-38.png');
-$('.js-7').attr('src', 'images/tu-pian-43.png');
+$('.js-5').attr('src', 'images/yan-se-40-1.png');
+$('.js-6').attr('src', 'images/sketch-38-1.png');
+$('.js-7').attr('src', 'images/guan-li-liu-zhuan-xie-zuo-43-1.png');
+$('.js-8').attr('src', 'images/tu-pian-43.png');
 
 }
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
